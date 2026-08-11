b78cdf748caff0e3125ffcff279b00bf00d731eb
André Barros <andrembcrypto@gmail.com>
2026-08-11 15:37:37 +0100

    chore(App): update title text

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 export default function App() {
-  return <div className="max-w-2x1 mx-auto p-4 flex flex-col gap-4">
+  return <div className="max-w-2x1 mx-auto p-8 flex flex-col gap-4">
     <Header />
     <Kanban />
   </div>
@@ -9,7 +9,10 @@ export default function App() {
 function Header() {
   return (
     <header className="flex justify-between items-center">
-      <div className="text-xl font-bold">Job Application Tracker</div>
+      <div className="text-xl font-bold">
+        <span className="text-[#e3e3e3]">Job Application</span>{' '}
+        <span className="text-[#2dbf8f]">Tracker</span>
+      </div>
       <div>
         <button className="bg-[#00A86B] hover:bg-[#00C27F] text-white font-bold py-2 px-4 border-b-4 border-[#00704A] hover:border-[#00915F] rounded">
  + Add New Application
